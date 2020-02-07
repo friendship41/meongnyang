f9b9709a1f33fce7a5a34588473fa027d287cfd4
stage41 <stage41@nate.com>
2020-02-07 16:23:03 +0900

    디비 접속 테스트 완료

--- src/test/java/com/ttest/go/MyDataSourceTest.java
+++ src/test/java/com/ttest/go/MyDataSourceTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml", "\"file:src/main/webapp/WEB-INF/spring/*.xml" })
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml"})
 public class MyDataSourceTest {
 	@Inject
 	private DataSource ds;
--- src/test/java/com/ttest/go/MyOracleConnectionTest.java
+++ src/test/java/com/ttest/go/MyOracleConnectionTest.java
@@ -2,20 +2,28 @@ package com.ttest.go;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import org.junit.Test;
 
 public class MyOracleConnectionTest {
 	private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	private static final String URL = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
-	private static final String ID = "stage41";
-	private static final String PASS = "qwe";
+	private static final String URL = "jdbc:oracle:thin:@meongnyang.c2aqelxnnbez.ap-northeast-2.rds.amazonaws.com:1521/mn";
+	private static final String ID = "meongnyang";
+	private static final String PASS = "vmfhwprxm";
 
 	@Test
 	public void testConnection() throws Exception {
 		Class.forName(DRIVER);
-		try (Connection conn = DriverManager.getConnection(URL, ID, PASS)) {
+		try
+		{
+			Connection conn = DriverManager.getConnection(URL, ID, PASS);
 			System.out.println(conn);
+			PreparedStatement pstmt = conn.prepareStatement("SELECT 30*30 FROM DUAL");
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			System.out.println(rs.getInt(1));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
