9cba9318ea2d6a3358fca4289b94263fb263f7e9
stage41 <stage41@nate.com>
2020-02-07 09:56:05 +0900

    INIT COMMIT 세팅 미완성
    완료사항
    1. maven / spring
    2. applicationContext 기본세팅
    3. admin-servlet 기본세팅
    4. client-servlet 기본세팅
    5. 큰 패키지 구조 완성
    6. admin/client  html 기본업로드
    
    작업예정
    1. 패키지 나누기
    2. HTML include로 헤더 푸터 나누기
    3. 리소스폴더 관련 작업

---
+++ src/main/java/com/mall/meongnyang/client/index/controller/indexController.java
@@ -0,0 +1,14 @@
+package com.mall.meongnyang.client.index.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+	@RequestMapping("/index.do")
+	public String indexPage()
+	{
+		return "index";
+	}
+}
