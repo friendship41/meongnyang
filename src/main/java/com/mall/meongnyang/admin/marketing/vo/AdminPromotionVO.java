fb162fafbe0e50873a480b788236a78ddb78bc67
youngs419 <sey4848@gmail.com>
2020-02-11 17:19:29 +0900

    adminPromotionVO 작성

--- src/main/java/com/mall/meongnyang/admin/marketing/vo/AdminPromotionVO.java
+++ src/main/java/com/mall/meongnyang/admin/marketing/vo/AdminPromotionVO.java
@@ -1,5 +1,44 @@
 package com.mall.meongnyang.admin.marketing.vo;
 
-public class AdminPromotionVO {
+import java.sql.Date;
 
+public class AdminPromotionVO {
+	private int promotionTbCode;
+	private String promotionTbName;
+	private String promotionTbContent;
+	private Date promotionTbStartDate;
+	private Date promotionTbEndDate;
+	
+	public int getPromotionTbCode() {
+		return promotionTbCode;
+	}
+	public void setPromotionTbCode(int promotionTbCode) {
+		this.promotionTbCode = promotionTbCode;
+	}
+	public String getPromotionTbName() {
+		return promotionTbName;
+	}
+	public void setPromotionTbName(String promotionTbName) {
+		this.promotionTbName = promotionTbName;
+	}
+	public String getPromotionTbContent() {
+		return promotionTbContent;
+	}
+	public void setPromotionTbContent(String promotionTbContent) {
+		this.promotionTbContent = promotionTbContent;
+	}
+	public Date getPromotionTbStartDate() {
+		return promotionTbStartDate;
+	}
+	public void setPromotionTbStartDate(Date promotionTbStartDate) {
+		this.promotionTbStartDate = promotionTbStartDate;
+	}
+	public Date getPromotionTbEndDate() {
+		return promotionTbEndDate;
+	}
+	public void setPromotionTbEndDate(Date promotionTbEndDate) {
+		this.promotionTbEndDate = promotionTbEndDate;
+	}
+	
+	
 }
