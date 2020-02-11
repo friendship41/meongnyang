8d9416f63b725de2d38e50f91aebfbe78a065717
youngs419 <sey4848@gmail.com>
2020-02-11 17:18:59 +0900

    adminPromotionDAO 작성

--- src/main/java/com/mall/meongnyang/admin/marketing/dao/AdminPromotionDAO.java
+++ src/main/java/com/mall/meongnyang/admin/marketing/dao/AdminPromotionDAO.java
@@ -1,5 +1,12 @@
 package com.mall.meongnyang.admin.marketing.dao;
 
-public interface AdminPromotionDAO {
+import java.util.List;
 
+import com.mall.meongnyang.admin.marketing.vo.AdminPromotionVO;
+
+public interface AdminPromotionDAO {
+	void deletePromotion(int promotionTbCode);
+	void insertPromotion(AdminPromotionVO adminPromotionVO);
+	List<AdminPromotionVO> selectPromotionList();
+	AdminPromotionVO selectPromotion(int promotionTbCode);
 }
--- src/main/java/com/mall/meongnyang/admin/marketing/dao/AdminPromotionDAOImpl.java
+++ src/main/java/com/mall/meongnyang/admin/marketing/dao/AdminPromotionDAOImpl.java
@@ -1,5 +1,37 @@
 package com.mall.meongnyang.admin.marketing.dao;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mall.meongnyang.admin.marketing.vo.AdminPromotionVO;
+
+@Repository
 public class AdminPromotionDAOImpl implements AdminPromotionDAO {
+
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	@Override
+	public void deletePromotion(int promotionTbCode) {
+		sqlSessionTemplate.delete("promotionDAO.deletePromotion", promotionTbCode);
+	}
+
+	@Override
+	public void insertPromotion(AdminPromotionVO adminPromotionVO) {
+		sqlSessionTemplate.insert("promotionDAO.insertPromotion", adminPromotionVO);
+	}
+
+	@Override
+	public List<AdminPromotionVO> selectPromotionList() {
+		return sqlSessionTemplate.selectList("promotionDAO.selectPromotionList");
+	}
+
+	@Override
+	public AdminPromotionVO selectPromotion(int promotionTbCode) {
+		return sqlSessionTemplate.selectOne("promotionDAO.selectPromotion", promotionTbCode);
+	}
 
 }
