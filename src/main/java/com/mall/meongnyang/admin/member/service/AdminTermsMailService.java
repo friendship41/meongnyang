391a0e9f08eb38a50b1ccb1e75bca1a158b59d6a
indeed851124 <kangholee8512@gmail.com>
2020-02-24 00:22:16 +0900

    약관 회원db에 있는 모든 회원에게 이메일전송 기능 추가 이메일 보낸 후 "이메일 발송되었습니다" alert 추가 수정 후 이메일 작성form에서 모든 내용 readonly로 변경 mypage의 사이드바에 wishlist 링크 걸어놈

--- src/main/java/com/mall/meongnyang/admin/member/service/AdminTermsMailService.java
+++ src/main/java/com/mall/meongnyang/admin/member/service/AdminTermsMailService.java
@@ -1,8 +1,11 @@
 package com.mall.meongnyang.admin.member.service;
 
+import java.util.List;
+
+import com.mall.meongnyang.admin.member.vo.AdminMemberVO;
 import com.mall.meongnyang.admin.member.vo.AdminTermsMailVO;
 
 public interface AdminTermsMailService {
-	public void send(AdminTermsMailVO mailVO);
+	public void send(AdminTermsMailVO mailVO, List<AdminMemberVO> memberList);
 	
 }
