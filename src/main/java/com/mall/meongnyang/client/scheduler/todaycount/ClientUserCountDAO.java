a889d1e53886505a50ef8561a94e3e0f93349910
stage41 <stage41@nate.com>
2020-02-24 20:31:32 +0900

    스케줄러를 이용한 자정에 방문수,가입수,탈퇴수 입력

---
+++ src/main/java/com/mall/meongnyang/client/scheduler/todaycount/ClientUserCountDAO.java
@@ -0,0 +1,6 @@
+package com.mall.meongnyang.client.scheduler.todaycount;
+
+public interface ClientUserCountDAO
+{
+    public void insertUserCount(ClientUserCountVO clientUserCountVO);
+}
---
+++ src/main/java/com/mall/meongnyang/client/scheduler/todaycount/InsertVisitMemberCountSchedulerService.java
@@ -0,0 +1,24 @@
+package com.mall.meongnyang.client.scheduler.todaycount;
+
+import com.mall.meongnyang.client.intercepter.visitCount.VisitCounter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InsertVisitMemberCountSchedulerService
+{
+    @Autowired
+    private VisitCounter visitCounter;
+    @Autowired
+    private ClientUserCountDAO clientUserCountDAO;
+
+    @Scheduled(cron = "59 59 23 * * *")
+    public void insertTodayCount()
+    {
+        ClientUserCountVO clientUserCountVO = new ClientUserCountVO();
+        clientUserCountVO.setUserCountTbVisitors(visitCounter.getVisitCount());
+        clientUserCountDAO.insertUserCount(clientUserCountVO);
+    }
+
+}
