bb8b9d806a00201f1a88817d44c18f4e4a061e9e
stage41 <stage41@nate.com>
2020-03-06 13:01:31 +0900

    각종 버그 및 설정 수정

--- src/main/java/com/mall/meongnyang/client/member/controller/ClientNaverLoginController.java
+++ src/main/java/com/mall/meongnyang/client/member/controller/ClientNaverLoginController.java
@@ -1,9 +1,9 @@
 package com.mall.meongnyang.client.member.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpSession;
-
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.mall.meongnyang.client.member.service.ClientNaverLoginService;
+import com.mall.meongnyang.client.member.service.ClientSelectLoginService;
+import com.mall.meongnyang.client.member.vo.ClientCustomerVO;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -14,10 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.github.scribejava.core.model.OAuth2AccessToken;
-import com.mall.meongnyang.client.member.service.ClientNaverLoginService;
-import com.mall.meongnyang.client.member.service.ClientSelectLoginService;
-import com.mall.meongnyang.client.member.vo.ClientCustomerVO;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 
 
@@ -46,23 +44,7 @@ public class ClientNaverLoginController {
 	private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
 		this.naverLoginBO = naverLoginBO;
 	}
-	
-	@RequestMapping(value = "/index.do", method = { RequestMethod.GET, RequestMethod.POST })
-	public String login(Model model, HttpSession session) {
-		
-		/* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출 */
-		String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
-		
-		//https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=sE***************&
-		//redirect_uri=http%3A%2F%2F211.63.89.90%3A8090%2Flogin_project%2Fcallback&state=e68c269c-5ba9-4c31-85da-54c16c658125
-		System.out.println("네이버:" + naverAuthUrl);
-		
-		//네이버 
-		model.addAttribute("url", naverAuthUrl);
 
-		/* 생성한 인증 URL을 View로 전달 */
-		return "index";
-	}
 	
 	@RequestMapping(value="/NaverLogin.do",method = { RequestMethod.GET, RequestMethod.POST })
 	public String NaverLogin(Model model, @RequestParam String code, @RequestParam String state, HttpSession session,ClientCustomerVO clientCustomerVO) 
