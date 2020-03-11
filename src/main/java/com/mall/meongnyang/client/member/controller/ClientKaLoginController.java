077807e7f8a11e7f1daea16429920c199575664a
stage41 <stage41@nate.com>
2020-03-11 21:38:07 +0900

    (완료)소셜로그인시 동의여부 테이블에 추가해주어야함

--- src/main/java/com/mall/meongnyang/client/member/controller/ClientKaLoginController.java
+++ src/main/java/com/mall/meongnyang/client/member/controller/ClientKaLoginController.java
@@ -2,11 +2,9 @@ package com.mall.meongnyang.client.member.controller;
 
 import com.mall.meongnyang.admin.marketing.vo.AdminEventAttendenceVO;
 import com.mall.meongnyang.admin.marketing.vo.AdminEventVO;
-import com.mall.meongnyang.client.member.service.ClientKaLoginService;
-import com.mall.meongnyang.client.member.service.ClientSelectAvailableEventService;
-import com.mall.meongnyang.client.member.service.ClientSelectEventAttendenceService;
-import com.mall.meongnyang.client.member.service.ClientSelectLoginService;
+import com.mall.meongnyang.client.member.service.*;
 import com.mall.meongnyang.client.member.vo.ClientCustomerVO;
+import com.mall.meongnyang.client.member.vo.ClientTermsAgreeVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,62 +15,77 @@ import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 
 @Controller
-public class ClientKaLoginController {
+public class ClientKaLoginController
+{
 
-	@Autowired
-	private ClientKaLoginService clientInsertKaLoginService;
+    @Autowired
+    private ClientKaLoginService clientInsertKaLoginService;
 
-	@Autowired
-	private ClientSelectLoginService clientSelectLoginService;
-	@Autowired
-	private ClientSelectAvailableEventService clientSelectAvailableEventService;
-	@Autowired
-	private ClientSelectEventAttendenceService clientSelectEventAttendenceService;
+    @Autowired
+    private ClientSelectLoginService clientSelectLoginService;
+    @Autowired
+    private ClientSelectAvailableEventService clientSelectAvailableEventService;
+    @Autowired
+    private ClientSelectEventAttendenceService clientSelectEventAttendenceService;
+    @Autowired
+    private ClientInsertTermsAgreeListService clientInsertTermsAgreeListService;
 
 
+    @RequestMapping(value = "/klogin.do")
+    public String klogin(@RequestParam("code") String code, HttpSession session, Model model)
+    {
+        HashMap<String, Object> token = clientInsertKaLoginService.getKakaoAccessToken(code);
 
-	@RequestMapping(value="/klogin.do")
-	public String klogin(@RequestParam("code") String code, HttpSession session, Model model) {
-		HashMap<String, Object> token = clientInsertKaLoginService.getKakaoAccessToken(code);
-		
-		ClientCustomerVO clientCustomerVO = clientInsertKaLoginService.getKakaoUserInfo(token);
-		ClientCustomerVO tempVO = clientSelectLoginService.selectLoginCheck(clientCustomerVO);
-		
-		// 최초 카카오톡 로그인 시 DB에 사용자 정보 삽입(회원가입)과 로그인을 동시에 처리
-		// else if = 최초 카카오톡 로그인이 아닌 경우 DB 조회 후 로그인만 처리 (AccessToken 갱신 포함)
-		// else = 카카오톡 로그인 실패
-		if (tempVO == null) {
-			clientInsertKaLoginService.insertKakao(clientCustomerVO);
-			session.setAttribute("customer", clientCustomerVO);
-			
-			return "index";
-		} else if(tempVO!=null && clientCustomerVO.getCustomerTbPassword()!=null) {
+        ClientCustomerVO clientCustomerVO = clientInsertKaLoginService.getKakaoUserInfo(token);
+        ClientCustomerVO tempVO = clientSelectLoginService.selectLoginCheck(clientCustomerVO);
+
+        // 최초 카카오톡 로그인 시 DB에 사용자 정보 삽입(회원가입)과 로그인을 동시에 처리
+        // else if = 최초 카카오톡 로그인이 아닌 경우 DB 조회 후 로그인만 처리 (AccessToken 갱신 포함)
+        // else = 카카오톡 로그인 실패
+        if (tempVO == null)
+        {
+            clientInsertKaLoginService.insertKakao(clientCustomerVO);
+			tempVO = clientSelectLoginService.selectLoginCheck(clientCustomerVO);
+            session.setAttribute("customer", tempVO);
+
+			ClientTermsAgreeVO clientTermsAgreeVO = new ClientTermsAgreeVO();
+			clientTermsAgreeVO.setTermsTbNo(2);
+			clientTermsAgreeVO.setCustomerTbNo(tempVO.getCustomerTbNo());
+			clientTermsAgreeVO.setTermsAgreeTbConsentStatus("N");
+            clientInsertTermsAgreeListService.insertTermsAgreeListService(clientTermsAgreeVO);
+
+            return "redirect:/index.do";
+        }
+        else if (tempVO != null && clientCustomerVO.getCustomerTbPassword() != null)
+        {
 //			TODO DB 사용자의 accessToken과 updateToken을 업데이트 하는 서비스 필요
 //			HashMap<String, Object> updateToken = clientInsertKaLoginService.updateKakaoAccessToken(clientCustomerVO);
-			session.setAttribute("customer", tempVO);
+            session.setAttribute("customer", tempVO);
 
-			AdminEventVO event = clientSelectAvailableEventService.selectAvailableEvent(new AdminEventVO());
-			if(event != null)
-			{
-				AdminEventAttendenceVO adminEventAttendenceVO = new AdminEventAttendenceVO();
-				adminEventAttendenceVO.setCustomerTbNo(tempVO.getCustomerTbNo());
-				adminEventAttendenceVO.setEventTbNo(event.getEventTbNo());
-				boolean isEventAttended = clientSelectEventAttendenceService.checkAttendenceToday(adminEventAttendenceVO);
-				if(isEventAttended)
-				{
-					session.setAttribute("eventGo","f");
-				}
-				else
-				{
-					session.setAttribute("eventGo","t");
-				}
-			}
-			
-			return "redirect:/index.do";
-		} else {
-			model.addAttribute("loginCheckSubmit", false);
-			return "redirect:/index.do";
-		}
-	}
-	
+            AdminEventVO event = clientSelectAvailableEventService.selectAvailableEvent(new AdminEventVO());
+            if (event != null)
+            {
+                AdminEventAttendenceVO adminEventAttendenceVO = new AdminEventAttendenceVO();
+                adminEventAttendenceVO.setCustomerTbNo(tempVO.getCustomerTbNo());
+                adminEventAttendenceVO.setEventTbNo(event.getEventTbNo());
+                boolean isEventAttended = clientSelectEventAttendenceService.checkAttendenceToday(adminEventAttendenceVO);
+                if (isEventAttended)
+                {
+                    session.setAttribute("eventGo", "f");
+                }
+                else
+                {
+                    session.setAttribute("eventGo", "t");
+                }
+            }
+
+            return "redirect:/index.do";
+        }
+        else
+        {
+            model.addAttribute("loginCheckSubmit", false);
+            return "redirect:/index.do";
+        }
+    }
+
 }
