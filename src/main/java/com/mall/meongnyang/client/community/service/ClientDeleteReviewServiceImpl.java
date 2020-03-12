65c3fb26a77f68166c7860fea386ffed52fade0e
youngs419 <sey4848@gmail.com>
2020-03-12 17:05:27 +0900

    삭제시 평점 수정되게 바꿈

--- src/main/java/com/mall/meongnyang/client/community/service/ClientDeleteReviewServiceImpl.java
+++ src/main/java/com/mall/meongnyang/client/community/service/ClientDeleteReviewServiceImpl.java
@@ -2,7 +2,9 @@ package com.mall.meongnyang.client.community.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mall.meongnyang.admin.product.vo.AdminProductSaleVO;
 import com.mall.meongnyang.client.community.dao.ClientReviewDAO;
 import com.mall.meongnyang.client.community.vo.ClientReviewVO;
 
@@ -13,8 +15,13 @@ public class ClientDeleteReviewServiceImpl implements ClientDeleteReviewService 
 	private ClientReviewDAO clientReviewDAO;
 
 	@Override
+	@Transactional
 	public void deleteReview(ClientReviewVO clientReviewVO) {
 		clientReviewDAO.deleteReview(clientReviewVO);
+		
+		AdminProductSaleVO adminProductSaleVO = new AdminProductSaleVO();
+		adminProductSaleVO.setProductTbCode(clientReviewVO.getProductTbCode());
+		clientReviewDAO.updateProductSaleRating(adminProductSaleVO);
 	}
 
 }
