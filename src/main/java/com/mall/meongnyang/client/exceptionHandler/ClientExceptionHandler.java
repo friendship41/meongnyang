8f9c3ec842d914170e842d9e2b827326e230a9c1
stage41 <stage41@nate.com>
2020-03-12 16:54:33 +0900

    log4j INFO이상 로그 파일 남기기 완료

---
+++ src/main/java/com/mall/meongnyang/client/exceptionHandler/ClientExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.mall.meongnyang.client.exceptionHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@ControllerAdvice("com.mall.meongnyang.client")
+public class ClientExceptionHandler
+{
+    @ExceptionHandler(Exception.class)
+    public ModelAndView allException(Exception e)
+    {
+        Logger logger = LoggerFactory.getLogger(e.getClass());
+        logger.error("-------------------Client Error-----------------");
+        logger.error(e.getMessage());
+        logger.error("------------------------------------------------");
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.setViewName("include/error");
+        return mav;
+    }
+}
